38a71c08ee1dad5e55a6791a2c0e3dc91ab4b540
adityadhopade <adityadhopade18@gmail.com>
2023-05-22 14:38:14 +0530

    Pushing the table contents via dump script

---
+++ script.sql
@@ -0,0 +1,5 @@
+INSERT INTO person(person_id, fname, lname, age) VALUES (1, 'Peter', 'Engineer', 32);
+INSERT INTO person(person_id, fname, lname, age) VALUES (2, 'Rob', 'Lion', 30);
+INSERT INTO person(person_id, fname, lname, age) VALUES (3, 'Ryan', 'Deer', 25);
+INSERT INTO person(person_id, fname, lname, age) VALUES (4, 'Harry', 'Ranger', 41);
+INSERT INTO person(person_id, fname, lname, age) VALUES (5, 'Zack', 'Knight', 20);
